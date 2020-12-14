87ba53c4ef1d287646ac44b661083145305ffe66
ChiefChippy2 <chiefchippy2@gmail.com>
2020-12-14 17:52:30 +0100

    Fix, and addition of safer polyfill

---
+++ src/update/polyfill2.js
@@ -0,0 +1,15 @@
+//get SB auctions isn't optimized
+//this is slower, but less error
+const Client = require("../API");
+module.exports = {
+    async getSkyblockAuctions(){
+        const auction0 = await Client.getSkyblockAuctions(0);
+        const auctions = {info:auction0.info,auctions:[auction0.auctions]}
+        for(let i =1;i<auction0.info.totalPages;i++){
+            let ac=await Client.getSkyblockAuctions(i);
+            auctions.auctions = auctions.auctions.concat(ac.auctions);
+            await new Promise(r=>setTimeout(r,2000));
+        }
+        return auctions;
+    }
+}
