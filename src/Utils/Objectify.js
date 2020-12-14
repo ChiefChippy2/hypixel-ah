30446e66aa8d4338bbf55a130bbcf1dd1cb5087b
ChiefChippy2 <chiefchippy2@gmail.com>
2020-12-14 17:31:45 +0100

    fix

--- src/Utils/Objectify.js
+++ src/Utils/Objectify.js
@@ -1,7 +1,7 @@
 module.exports = function O(classObj){
- return Object.getOwnPropertyNames(classObj).reduce((a, b) => {
-    if(Array.isArray(classObj[b])) return classObj[b].map(x=>typeof x==="Function"?O(x):x);
-    a[b] = typeof classObj[b] === "object" ? JSON.stringify(O(classObj[b])) : classObj[b];
-        return a;
-    }, {});
-}
+    return Object.getOwnPropertyNames(classObj).reduce((a, b) => {
+       if(Array.isArray(classObj[b])) a[b]= classObj[b].map(x=>typeof x==="Function"?O(x):x);
+       else a[b] = typeof classObj[b] === "object" ? JSON.stringify(O(classObj[b])) : classObj[b];
+           return a;
+       }, {});
+   }
