5e14a10c7c6e9957a9fc94ab2fa0ca3c4b9db1ca
chief <chief>
2020-12-18 10:15:04 +0000

    even more features

--- src/ws/APIresponse.js
+++ src/ws/APIresponse.js
@@ -1,6 +1,6 @@
 module.exports = class APIResponse{
     constructor(data,req){
-        this.response = JSON.stringify({...data,...req})
+        this.response = JSON.stringify({...data,req})
     }
     toSendable(){
         return this.response
